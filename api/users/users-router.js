811473439754d6970637f44f640fe554c3e8f46e
brepp22 <brepp@hotmail.com>
2024-07-12 21:23:35 -0700

    project updates

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,30 +1,44 @@
-const express = require('express');
-const { validateUserId,
+const express = require('express')
 
-} = require('../middleware/middleware')
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
+
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware')
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+ // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(error => {
+    res.status(500).json({
+      message: 'Cannot get users'
+    })
+  })
 });
+
 
 router.get('/:id', validateUserId , (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.name)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
@@ -37,10 +51,12 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user)
+  console.log(req.text)
 });
 
 // do not forget to export the router
